5d34343ea4be46c9d3c68f6c9ce43c61ca750afb
Gerrino <g3rrino@gmail.com>
2023-07-15 00:00:11 +0200

    Update init.py

---
+++ failure_recognition_signal_processing/__init__.py
@@ -0,0 +1,14 @@
+DEFAULT_MAX_INT = 100
+DEFAULT_MIN_INT = 0
+DEFAULT_MAX_FLOAT = 10
+DEFAULT_MIN_FLOAT = -10
+DEFAULT_INT = 1
+DEFAULT_FLOAT = 0.1
+
+PATH_DICT = {
+    "features": "./examples/tsfresh_features.json",
+    "forest_params": "./examples/tsfresh_random_forest.json",
+    "timeSeries": "./examples/NASA_tsfresh_format_all.csv",
+    "label": "./examples/label.csv",
+    "testSettings": "./examples/testsettings.csv",
+}
